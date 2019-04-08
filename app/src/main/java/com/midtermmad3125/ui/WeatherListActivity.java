c8277c2643eda204f3740c6c7aad158878f82d20
samir-dell <samirthaker2020>
2019-04-08 19:18:10 -0400

    reading json file

--- app/src/main/java/com/midtermmad3125/ui/WeatherListActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/WeatherListActivity.java
@@ -2,16 +2,73 @@ package com.midtermmad3125.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.midtermmad3125.R;
 
-public class WeatherListActivity extends AppCompatActivity
-{
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class WeatherListActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_list);
+    }
+    public String loadJSONFromAsset() {
+        String json;
+        try {
+            InputStream is = getAssets().open("moscow_weather.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+    private void processJSON() {
+        String js = loadJSONFromAsset();
+        if (js != null) {
+            // Log.d("json",js);
+            try {
+                JSONArray mJSONArray = new JSONArray(js);
+                studentArrayList = new ArrayList<>();
+                for (int i = 0; i < mJSONArray.length(); i++) {
+
+                    JSONObject mJSONObj = mJSONArray.getJSONObject(i);
+                    //  Log.d("mjson", mJSONObj.toString());
+                    if (mJSONObj.has("sid")) {
+                        String id = mJSONObj.getString("sid");
+                        String sname = mJSONObj.getString("sname");
+                        String gender = mJSONObj.getString("gender");
+                        Log.d("mjson_ID", String.valueOf(id));
+                        Log.d("mjson_name", String.valueOf(sname));
+                        Log.d("mjson_gender", String.valueOf(gender));
+                        studentArrayList.add(new Student(String.valueOf(id), String.valueOf(sname), String.valueOf(gender)));
+
+
+                    }
+
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
     }
 }
--- app/src/main/java/com/midtermmad3125/utils/ReadJSONUtils.java
+++ app/src/main/java/com/midtermmad3125/utils/ReadJSONUtils.java
@@ -24,4 +24,6 @@ public class ReadJSONUtils
         }
         return jsonString;
     }
+
+
 }
