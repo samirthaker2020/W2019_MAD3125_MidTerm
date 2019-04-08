1c2249bdf5d9b9c487b0b137a93d41306a71f7d1
samir-dell <samirthaker2020>
2019-04-08 18:43:21 -0400

    splash screen created

---
+++ app/src/main/java/com/midtermmad3125/ui/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.midtermmad3125.ui;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.midtermmad3125.R;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent LoginIntent = new Intent(SplashScreen.this,MainCityActivity.class);
+                startActivity(LoginIntent);
+                finish();
+
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
