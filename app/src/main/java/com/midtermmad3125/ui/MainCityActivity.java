c3ea1c8720023650105818ecb9188bbf72ebb3e7
samir-dell <samirthaker2020>
2019-04-08 20:05:10 -0400

    display country

--- app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
@@ -2,12 +2,17 @@ package com.midtermmad3125.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.midtermmad3125.R;
+import com.midtermmad3125.utils.ReadJSONUtils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainCityActivity extends AppCompatActivity
 {
-
+private TextView cityname,lati,longi,population;
 
 
     @Override
@@ -16,7 +21,31 @@ public class MainCityActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        cityname=findViewById(R.id.cityname);
+        lati=findViewById(R.id.latitiude);
+        longi=findViewById(R.id.logitude);
+        population=findViewById(R.id.population);
+        ProcessData();
 
+    }
+    public void ProcessData (){
+        String JsonData = ReadJSONUtils.loadJSONFromAsset(this,"moscow_weather.json");
+
+        try {
+            JSONObject cityData = new JSONObject(JsonData);
+            JSONObject cityObject= cityData.getJSONObject("city");
+            cityname.setText(cityObject.getString("name"));
+
+            JSONObject cityCoordObject= cityObject.getJSONObject("coord");
+
+            lati.setText("Latitude: "+cityCoordObject.getString("lat"));
+            longi.setText("Longitude: "+cityCoordObject.getString("lon"));
+
+          //  citycode.setText("Country: "+cityObject.getString("country"));
+            population.setText("population: "+cityObject.getString("population"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
